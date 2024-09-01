f94870671710747f77094be270c311949977bfc8
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2024-09-01 18:45:59 +0530

    Update App_Asecnd_.py

--- App_Asecnd_.py
+++ App_Asecnd_.py
@@ -241,6 +241,7 @@ multivariate_charts.plotly_chart(dem_subs_chart)
 
 
 ##top 100 apps
+st.dataframe(data)
 top_100 = data.sort_values(by='Impact_Factor',ascending=False).head(100)
 top_100 = top_100[['Application','Reviews','Downloads','Impact_Factor','Genres','Content Rating','File Size']]
 st.write('Top 100 Applications')
